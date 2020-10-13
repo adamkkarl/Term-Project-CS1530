a01cba12fea8d29357e25a75a0bf20f8d69c2884
qhv1 <46951778+qhv1@users.noreply.github.com>
2020-10-13 16:01:39 -0400

    Update SharklineSchema.sql

--- db/SharklineSchema.sql
+++ db/SharklineSchema.sql
@@ -1,7 +1,9 @@
 USE sharklineDB;
-# newUsers table only includes accounts that need to be verified. After they
-# are verified, they are placed into the accounts. Most of the FOREIGN KEYS
-# for the other tables reference this accounts table.
+# accounts table includes information on all the accounts,
+# whether they are verified or not (marked by the verification
+# variable, 1 = verified, 0 = not verified). For future reference
+# for all img files they relate to a filepath, not the image itself.
+# All passwords stored are hashed.
 CREATE TABLE accounts (
 	account_email VARCHAR(50) NOT NULL PRIMARY KEY,
     account_name VARCHAR(50) NOT NULL UNIQUE,
@@ -11,6 +13,9 @@ CREATE TABLE accounts (
     img_proof VARCHAR(1024) NOT NULL
 );
 
+# business_accounts includes information related to business accounts.
+# The business_email and business_name attributes are foreign keys which
+# reference the accounts table with the respective account_email and account_name.
 CREATE TABLE business_accounts (
 	business_email VARCHAR(50) NOT NULL PRIMARY KEY,
     business_name VARCHAR(50) NOT NULL UNIQUE,
@@ -30,6 +35,10 @@ CREATE TABLE business_accounts (
     FOREIGN KEY (business_name) REFERENCES accounts(account_name)
 );
 
+# company_project table stores all the data entries for the company
+# projects they would like to list on their account page. The business_email
+# references the business_accounts table and its respective attribute business_email.
+
 CREATE TABLE company_project (
 	business_email VARCHAR(50) NOT NULL,
     project_name VARCHAR(100) NOT NULL PRIMARY KEY,
@@ -38,6 +47,9 @@ CREATE TABLE company_project (
     FOREIGN KEY (business_email) REFERENCES business_accounts(business_email)
 );
 
+# investor_accounts table stores all the data for investor type accounts.
+# investor_email and investor_name attributes reference the table attributes
+# of accounts, with the respective account_email and account_name.
 CREATE TABLE investor_accounts (
 	investor_email VARCHAR(50) NOT NULL PRIMARY KEY,
     investor_name VARCHAR(50) NOT NULL UNIQUE,
@@ -54,6 +66,10 @@ CREATE TABLE investor_accounts (
     FOREIGN KEY (investor_name) REFERENCES accounts(account_name)
 );
 
+# account_connections table refers to currently matched companies
+# and investors on the website. The foreign keys are referenced by
+# their respective tables.
+
 CREATE TABLE account_connections (
 	business_name VARCHAR(50) NOT NULL UNIQUE,
     business_email VARCHAR(50) NOT NULL,
@@ -68,6 +84,12 @@ CREATE TABLE account_connections (
     FOREIGN KEY (investor_email) REFERENCES investor_accounts(investor_email)
 );
 
+# chat_log stores the information regarding messages sent
+# between parties on the website. sender_name and recipient_name
+# can either be from a business or investor, and as such reference
+# both tables. Further work may need to be done to ensure that 
+# the table cannot take a sender and recipient from the same table,
+# IE an investor sending a message to another investor.
 CREATE TABLE chat_log (
 	connection_id VARCHAR(36) NOT NULL PRIMARY KEY,
     sender_name VARCHAR(50) NOT NULL,
