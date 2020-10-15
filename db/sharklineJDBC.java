445f1d25f1cc4ea7476e4129438ce0e152c9622f
qhv1 <qhv1@pitt.edu>
2020-10-15 16:09:20 -0400

    Initial commit of sharklineJDBC file

---
+++ db/sharklineJDBC.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.sql.*;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+/**
+* This file processes all Java Database Connectivity code. Use the main
+* method to test some of the queries you write, but otherwise any
+* query you write make sure to create a method for it with documentation!
+*
+* Also, you probably will need to install the MySQL server from the workbench.
+* Link is provided here:  https://dev.mysql.com/downloads/installer/
+* Youtube for additional help on installation: https://www.youtube.com/watch?v=kEnD_KN7P-k&t=12s
+**/
+
+public class sharklineJDBC
+{
+  public static void main(String[] args)
+  {
+
+  }
+}
