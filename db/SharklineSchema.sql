49bc8a68ab392d94f52d4b5c4e1e32f4f946d0c7
adamkkarl <adamkkarl@gmail.com>
2020-10-19 17:24:29 -0400

    minor adjustments and typo fixes

--- db/SharklineSchema.sql
+++ db/SharklineSchema.sql
@@ -7,7 +7,7 @@ USE sharklineDB;
 CREATE TABLE accounts (
 	account_email VARCHAR(50) NOT NULL PRIMARY KEY,
     account_name VARCHAR(50) NOT NULL UNIQUE,
-    account_password VARCHAR(15) NOT NULL UNIQUE,
+    account_password VARCHAR(25) NOT NULL UNIQUE,
     type ENUM('Investor', 'Business'),
     verification TINYINT NOT NULL,
     img_proof VARCHAR(1024) NOT NULL
@@ -24,13 +24,13 @@ CREATE TABLE business_accounts (
     size ENUM ('1-10', '11-30', '31-50', '51-100', '101-200', '200+'),
     established YEAR,
     investment_Range_Init INT UNSIGNED CHECK (investment_Range_Init > 1000),
-    investment_RangeE_nd INT UNSIGNED,
+    investment_Range_End INT UNSIGNED,
     website VARCHAR(2083),
     name_CEO VARCHAR(80),
     industry ENUM('Industrial', 'Health', 'Software/Tech', 'Entertainment', 'Food', 'Retail',
     'Finance', 'Marketing', 'Sales', 'Automotive', 'Education', 'Law', 'Hotel', 'Travel',
     'Energy', 'Environment', 'Transportation', 'Other') NOT NULL,
-    
+
     FOREIGN KEY (business_email) REFERENCES accounts(account_email),
     FOREIGN KEY (business_name) REFERENCES accounts(account_name)
 );
@@ -43,7 +43,7 @@ CREATE TABLE company_project (
 	business_email VARCHAR(50) NOT NULL,
     project_name VARCHAR(100) NOT NULL PRIMARY KEY,
     project_description VARCHAR(1500),
-    
+
     FOREIGN KEY (business_email) REFERENCES business_accounts(business_email)
 );
 
@@ -54,14 +54,9 @@ CREATE TABLE investor_accounts (
 	investor_email VARCHAR(50) NOT NULL PRIMARY KEY,
     investor_name VARCHAR(50) NOT NULL UNIQUE,
     investor_description VARCHAR(1000),
-    logo VARCHAR(1024),
-    size ENUM ('1-10', '11-30', '31-50', '51-100', '101-200', '200+'),
-    established YEAR,
     investment_Range_Init INT UNSIGNED CHECK (investment_Range_Init > 1000),
-    investment_RangeE_nd INT UNSIGNED,
-    website VARCHAR(2083),
-    name_CEO VARCHAR(80),
-    
+    investment_Range_End INT UNSIGNED,
+
     FOREIGN KEY (investor_email) REFERENCES accounts(account_email),
     FOREIGN KEY (investor_name) REFERENCES accounts(account_name)
 );
@@ -77,7 +72,7 @@ CREATE TABLE account_connections (
     investor_email VARCHAR(50) NOT NULL,
     date_connected DATE,
     connection_id VARCHAR(36) NOT NULL UNIQUE PRIMARY KEY,
-    
+
     FOREIGN KEY (business_name) REFERENCES business_accounts(business_name),
     FOREIGN KEY (business_email) REFERENCES business_accounts(business_email),
     FOREIGN KEY (investor_name) REFERENCES investor_accounts(investor_name),
@@ -87,7 +82,7 @@ CREATE TABLE account_connections (
 # chat_log stores the information regarding messages sent
 # between parties on the website. sender_name and recipient_name
 # can either be from a business or investor, and as such reference
-# both tables. Further work may need to be done to ensure that 
+# both tables. Further work may need to be done to ensure that
 # the table cannot take a sender and recipient from the same table,
 # IE an investor sending a message to another investor.
 CREATE TABLE chat_log (
@@ -96,12 +91,10 @@ CREATE TABLE chat_log (
     recipient_name VARCHAR(50) NOT NULL,
     message VARCHAR(900) NOT NULL,
     datetime_sent DATETIME,
-    
+
     FOREIGN KEY (connection_id) REFERENCES account_connections(connection_id),
     FOREIGN KEY (sender_name) REFERENCES account_connections(business_name),
     FOREIGN KEY (sender_name) REFERENCES account_connections(investor_name),
     FOREIGN KEY (recipient_name) REFERENCES account_connections(business_name),
     FOREIGN KEY (recipient_name) REFERENCES account_connections(investor_name)
 );
-
-
