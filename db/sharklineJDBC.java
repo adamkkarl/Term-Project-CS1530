d9d64b9ab93cbe61e528939cc991478b1669fa84
qhv1 <qhv1@pitt.edu>
2020-10-22 17:43:59 -0400

    Finished findAccount method

--- db/sharklineJDBC.java
+++ db/sharklineJDBC.java
@@ -28,9 +28,14 @@ import java.time.LocalDateTime;
 *
 * If you have any questions, just ask me (Quinn) in the discord or something.
 **/
-
 public class sharklineJDBC
 {
+  public enum Type
+  {
+    INVESTOR,
+    BUSINESS;
+  }
+
   //You'll need to fill this out for your own server MySQL for it
   //To work on your machine
   private static String username;
@@ -54,6 +59,12 @@ public class sharklineJDBC
       //Test your written JDBC methods bellow here.
       //Make sure to add some test data in the database or the ResultSets won't
       //have anything in them!
+      Account test = findAccount("fraudulentEmail@hotmail.com");
+      System.out.println(test.account_name);
+      if(findAccount("gaghksagkhj") == null)
+        System.out.println("Invalid email!");
+      if(findAccount("DROP TABLE") == null)
+        System.out.println("SQL Error!");
     }
     catch(Exception e)
     {
@@ -61,12 +72,64 @@ public class sharklineJDBC
     }
   }
   //Database Query Methods begin here
+  //(try to follow comment style of findAccount method)
   //----------------------------------------------------------------------------
-  public static Account findAccount(String name, String email) //WIP
+
+  /**
+  * findAccount searches through account table to find matching tuple
+  * with given email
+  *
+  * @param email the email we use to search the table for the tuple
+  * @return      Account object with all data associated with account tuple,
+  *              EXCEPT PASSWORD, returns null otherwise
+  *
+  *
+  */
+  public static Account findAccount(String email)
   {
-    PreparedStatement st = dbcon.prepareStatement("SELECT * FROM ");
+    try
+    {
+      PreparedStatement st =
+      dbcon.prepareStatement("SELECT * FROM accounts WHERE account_email= ?");
+      st.setString(1, email);
+
+      ResultSet result = st.executeQuery();
+      if(!(result.next()))
+        return null;
+
+      //parsing data from account table into relevenat fields into Account object
+      //EXCEPT PASSWORD
+      Account retrievedAccount = new Account();
+      retrievedAccount.account_email = email;
+      retrievedAccount.account_name = result.getString("account_name");
+      retrievedAccount.img_path = result.getString("img_proof");
+      if(result.getString("type").equals("Investor"))
+        retrievedAccount.accountType = Type.INVESTOR;
+      else
+        retrievedAccount.accountType = Type.BUSINESS;
+
+      if(result.getInt("verification") == 0)
+        retrievedAccount.isVerified = false;
+      else
+        retrievedAccount.isVerified = true;
+
+      st.close();
+      return retrievedAccount;
+    }
+    catch(SQLException e1)
+    {
+      while (e1 != null)
+      {
+        System.out.println("Message = " + e1.getMessage());
+        System.out.println("SQLErrorCode = " + e1.getErrorCode());
+        System.out.println("SQLState = " + e1.getSQLState());
+
+        e1 = e1.getNextException();
+      }
+      return null;
+    }
   }
-  private class Account //Might make this into a seperate file later, but for
+  private static class Account //Might make this into a seperate file later, but for
                         //for now ill keep it as a private class
   {
     public String account_email;
@@ -74,10 +137,7 @@ public class sharklineJDBC
     public String account_password;
     public boolean isVerified;
     public String img_path;
-    public enum type
-    {
-      INVESTOR,
-      BUSINESS;
-    }
+    public Type accountType;
+
   }
 }
