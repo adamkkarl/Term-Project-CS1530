7f1ee60180402fbebcde7819234d5a3afe8d9acd
qhv1 <qhv1@pitt.edu>
2020-10-29 16:33:20 -0400

    Made account class its own file

---
+++ db/Account.java
@@ -0,0 +1,81 @@
+public static class Account
+{
+  public enum Type
+  {
+    INVESTOR,
+    BUSINESS;
+  }
+  private String accountEmail;
+  private String accountName;
+  private String accountPassword;
+  private boolean isVerified;
+  private String imgPath;
+  private Type accountType;
+
+  public Account(String accountEmail, String accountName,
+                 String accountPassword, boolean isVerified
+                 String imgPath, Type accountType)
+  {
+    this.accountEmail = accountEmail;
+    this.accountName = accountName;
+    this.accountPassword = accountPassword;
+    this.isVerified = isVerified;
+    this.imgPath = imgPath;
+    this.accountType = accountType;
+  }
+
+  public String getEmail()
+  {
+    return accountEmail;
+  }
+  public String getName()
+  {
+    return accountName;
+  }
+  public String getPassword()
+  {
+    return accountPassword;
+  }
+  public boolean checkVerified()
+  {
+    return isVerified;
+  }
+  public boolean getImagePath()
+  {
+    return imgPath;
+  }
+  public Type getType()
+  {
+    return accountType;
+  }
+  public boolean setEmail(String email)
+  {
+    accountEmail = email;
+    return accountEmail.equals(email);
+  }
+  public boolean setName(String name)
+  {
+    accountName = name;
+    return accountName.equals(name);
+  }
+  public boolean setPassword(String password)
+  {
+    accountPassword = password;
+    return accountPassword.equals(password);
+  }
+  public boolean verify()
+  {
+    isVerified = true;
+    return isVerified;
+  }
+  public boolean setImagePath(String img)
+  {
+    imgPath = img;
+    return imgPath.equals(img);
+  }
+  public boolean setType(Type type)
+  {
+    accountType = type;
+    return accountType == type;
+  }
+}
