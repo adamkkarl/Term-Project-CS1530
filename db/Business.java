2096742fc5701c1dc35a942763bd45dc0fe300ff
aaronmathew589 <59574983+aaronmathew589@users.noreply.github.com>
2020-10-30 13:13:00 -0400

    Add files via upload

---
+++ db/Business.java
@@ -0,0 +1,161 @@
+import jdbcdemo.sharklineJDBC.*;
+
+
+public class Business {
+
+private String businessEmail;
+private String businessName;
+private String description;
+private String businessAbstract;
+private String logoPath;
+private Size size;
+private int year;
+private int investmentAsk;
+private int equityOffer;
+private String website;
+private String ceoName;
+private Industry businessIndustry;	
+
+	
+public Business(String businessEmail, String businessName,
+		String description, String businessAbstract, String logoPath,
+		Size size, int year, int investmentAsk, int equityOffer,
+		String website, String ceoName, Industry businessIndustry) {
+	
+	this.businessEmail = businessEmail;
+	this.businessName = businessName;
+	this.description = description;
+	this.businessAbstract = businessAbstract;
+	this.logoPath = logoPath;
+	this.size = size;
+	this.year = year;
+	this.investmentAsk = investmentAsk;
+	this.equityOffer = equityOffer;
+	this.website = website;
+	this.ceoName = ceoName;
+	this.businessIndustry = businessIndustry;
+	
+	
+}
+
+
+public String getBusinessEmail() {
+	return businessEmail;
+}
+
+
+public void setBusinessEmail(String businessEmail) {
+	this.businessEmail = businessEmail;
+}
+
+
+public String getBusinessName() {
+	return businessName;
+}
+
+
+public void setBusinessName(String businessName) {
+	this.businessName = businessName;
+}
+
+
+public String getDescription() {
+	return description;
+}
+
+
+public void setDescription(String description) {
+	this.description = description;
+}
+
+
+public String getBusinessAbstract() {
+	return businessAbstract;
+}
+
+
+public void setBusinessAbstract(String businessAbstract) {
+	this.businessAbstract = businessAbstract;
+}
+
+
+public String getLogoPath() {
+	return logoPath;
+}
+
+
+public void setLogoPath(String logoPath) {
+	this.logoPath = logoPath;
+}
+
+
+public Size getSize() {
+	return size;
+}
+
+
+public void setSize(Size size) {
+	this.size = size;
+}
+
+
+public int getYear() {
+	return year;
+}
+
+
+public void setYear(int year) {
+	this.year = year;
+}
+
+
+public int getInvestmentAsk() {
+	return investmentAsk;
+}
+
+
+public void setInvestmentAsk(int investmentAsk) {
+	this.investmentAsk = investmentAsk;
+}
+
+
+public int getEquityOffer() {
+	return equityOffer;
+}
+
+
+public void setEquityOffer(int equityOffer) {
+	this.equityOffer = equityOffer;
+}
+
+
+public String getWebsite() {
+	return website;
+}
+
+
+public void setWebsite(String website) {
+	this.website = website;
+}
+
+
+public String getCeoName() {
+	return ceoName;
+}
+
+
+public void setCeoName(String ceoName) {
+	this.ceoName = ceoName;
+}
+
+
+public Industry getBusinessIndustry() {
+	return businessIndustry;
+}
+
+
+public void setBusinessIndustry(Industry businessIndustry) {
+	this.businessIndustry = businessIndustry;
+}
+
+}
---
+++ db/Investor.java
@@ -0,0 +1,47 @@
+
+
+public class Investor {
+private String investor_email;
+private String investor_name;
+private String investor_description;
+private int investment_Range_Init;
+private int investment_Range_End;
+public String getInvestor_email() {
+	return investor_email;
+}
+public void setInvestor_email(String investor_email) {
+	this.investor_email = investor_email;
+}
+public String getInvestor_name() {
+	return investor_name;
+}
+public void setInvestor_name(String investor_name) {
+	this.investor_name = investor_name;
+}
+public String getInvestor_description() {
+	return investor_description;
+}
+public void setInvestor_description(String investor_description) {
+	this.investor_description = investor_description;
+}
+public int getInvestment_Range_Init() {
+	return investment_Range_Init;
+}
+public void setInvestment_Range_Init(int investment_Range_Init) {
+	this.investment_Range_Init = investment_Range_Init;
+}
+public int getInvestment_Range_End() {
+	return investment_Range_End;
+}
+public void setInvestment_Range_End(int investment_Range_End) {
+	this.investment_Range_End = investment_Range_End;
+}
+
+
+
+
+
+
+
+
+}
