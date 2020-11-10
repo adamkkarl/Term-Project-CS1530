34039af02c9e3516d54d38076f7bdc54cbea22cb
Luke Leiby <lucaswleiby@gmail.com>
2020-11-10 15:27:13 -0500

    Added logout functionality

---
+++ webapp/sharkline/WEB-INF/src/logout.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+public class logout extends HttpServlet
+{
+
+@Override
+public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+{
+HttpSession session = request.getSession(true);
+session.invalidate();
+RequestDispatcher rd = request.getRequestDispatcher("index.html");
+rd.forward(request, response);
+}
+}
